deca885ae32736b364364bb7a6a39c5cfba4c749
Amir Omidi <amir@aaomidi.com>
2018-03-10 15:50:02 -0500

    At responses to moves taken.

--- src/main/java/io/github/cs451/ge/adapter/CheckersUIResponse.java
+++ src/main/java/io/github/cs451/ge/adapter/CheckersUIResponse.java
@@ -11,12 +11,17 @@ public class CheckersUIResponse {
     private final boolean success;
     private final ResponseType responseType;
 
+    @RequiredArgsConstructor
+    @Getter
     public enum ResponseType {
-        SUCCESS,
-        INVALID_TURN,
-        INVALID_SELECTION,
-        INVALID_MOVE,
-        HAVE_TO_ATTACK,
-        MUST_COMPLETE_JUMPS;
+
+        SUCCESS("Successful!"),
+        INVALID_TURN("It isn't your turn!"),
+        INVALID_SELECTION("You can't select that."),
+        INVALID_MOVE("That's not a valid move."),
+        HAVE_TO_ATTACK("You have to attack."),
+        MUST_COMPLETE_JUMPS("You must complete your attack jumps.");
+        private final String message;
+
     }
 }
